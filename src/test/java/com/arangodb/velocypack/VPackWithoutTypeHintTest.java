590693baa53683f05883237ad72a009837e20c18
Michele Rastelli <rashtao@gmail.com>
2020-07-27 13:23:22 +0200

    parameterized useTypeHint tests

--- src/test/java/com/arangodb/velocypack/VPackWithoutTypeHintTest.java
+++ src/test/java/com/arangodb/velocypack/VPackWithoutTypeHintTest.java
@@ -21,6 +21,8 @@
 package com.arangodb.velocypack;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 
 import java.util.*;
 
@@ -30,7 +32,42 @@ import static org.hamcrest.Matchers.*;
 /**
  * @author Michele Rastelli
  */
+@RunWith(Parameterized.class)
 public class VPackWithoutTypeHintTest {
+
+	private final boolean useTypeHint;
+
+	@Parameterized.Parameters
+	public static Collection<Boolean> useTypeHint() {
+		return Arrays.asList(true, false);
+	}
+
+	public VPackWithoutTypeHintTest(final boolean useTypeHint) {
+		this.useTypeHint = useTypeHint;
+	}
+
+	public static class NestedIterable {
+		public Iterable<String> value;
+	}
+
+	@Test
+	public void nestedIterableWithoutTypeInformation() {
+		NestedCollection input = new NestedCollection();
+		input.value = Arrays.asList("a", "b", "c");
+
+		final VPack vpack = new VPack.Builder().useTypeHint(useTypeHint).build();
+		final VPackSlice slice = vpack.serialize(input);
+
+		assertThat(slice.isObject(), is(true));
+		assertThat(slice.get("value").isArray(), is(true));
+		if (!useTypeHint)
+			assertThat(slice.get("_class").isNone(), is(true));
+
+		NestedIterable output = vpack.deserialize(slice, NestedIterable.class);
+		assertThat(output.value, instanceOf(List.class));
+
+		assertThat(output.value, equalTo(input.value));
+	}
 
 	public static class NestedCollection {
 		public Collection<String> value;
@@ -41,12 +78,13 @@ public class VPackWithoutTypeHintTest {
 		NestedCollection input = new NestedCollection();
 		input.value = Arrays.asList("a", "b", "c");
 
-		final VPack vpack = new VPack.Builder().useTypeHint(false).build();
+		final VPack vpack = new VPack.Builder().useTypeHint(useTypeHint).build();
 		final VPackSlice slice = vpack.serialize(input);
 
 		assertThat(slice.isObject(), is(true));
 		assertThat(slice.get("value").isArray(), is(true));
-		assertThat(slice.get("_class").isNone(), is(true));
+		if (!useTypeHint)
+			assertThat(slice.get("_class").isNone(), is(true));
 
 		NestedCollection output = vpack.deserialize(slice, NestedCollection.class);
 		assertThat(output.value, instanceOf(List.class));
@@ -60,17 +98,21 @@ public class VPackWithoutTypeHintTest {
 
 	@Test
 	public void nestedMapWithoutTypeInformation() {
-		NestedMap input = new NestedMap();
-		input.value = Collections.singletonMap("a", "b");
+		Map<String, String> map = new HashMap<>();
+		map.put("a", "b");
 
-		final VPack vpack = new VPack.Builder().useTypeHint(false).build();
+		NestedMap input = new NestedMap();
+		input.value = map;
+
+		final VPack vpack = new VPack.Builder().useTypeHint(useTypeHint).build();
 		final VPackSlice slice = vpack.serialize(input);
 
 		assertThat(slice.isObject(), is(true));
 		assertThat(slice.get("value").isObject(), is(true));
-		assertThat(slice.get("_class").isNone(), is(true));
-		assertThat(slice.get("value").get("_class").isNone(), is(true));
-
+		if (!useTypeHint) {
+			assertThat(slice.get("_class").isNone(), is(true));
+			assertThat(slice.get("value").get("_class").isNone(), is(true));
+		}
 		NestedMap output = vpack.deserialize(slice, NestedMap.class);
 		assertThat(output.value, instanceOf(Map.class));
 
@@ -82,21 +124,44 @@ public class VPackWithoutTypeHintTest {
 	}
 
 	@Test
-	public void nestedObjectWithoutTypeInformation() {
+	public void nestedObjectListWithoutTypeInformation() {
 		NestedObject input = new NestedObject();
-		input.value = Collections.singletonMap("a", "b");
+		input.value = Arrays.asList("a", "b", "c");
 
-		final VPack vpack = new VPack.Builder().useTypeHint(false).build();
+		final VPack vpack = new VPack.Builder().useTypeHint(useTypeHint).build();
 		final VPackSlice slice = vpack.serialize(input);
+		assertThat(slice.isObject(), is(true));
+		assertThat(slice.get("value").isArray(), is(true));
+		if (!useTypeHint)
+			assertThat(slice.get("_class").isNone(), is(true));
 
+		NestedObject output = vpack.deserialize(slice, NestedObject.class);
+		assertThat(output.value, instanceOf(List.class));
+
+		assertThat(output.value, equalTo(input.value));
+	}
+
+	@Test
+	public void nestedObjectMapWithoutTypeInformation() {
+		Map<String, String> map = new HashMap<>();
+		map.put("a", "b");
+
+		NestedObject input = new NestedObject();
+		input.value = map;
+
+		final VPack vpack = new VPack.Builder().useTypeHint(useTypeHint).build();
+		final VPackSlice slice = vpack.serialize(input);
 		assertThat(slice.isObject(), is(true));
 		assertThat(slice.get("value").isObject(), is(true));
-		assertThat(slice.get("_class").isNone(), is(true));
-		assertThat(slice.get("value").get("_class").isNone(), is(true));
+		if (!useTypeHint) {
+			assertThat(slice.get("_class").isNone(), is(true));
+			assertThat(slice.get("value").get("_class").isNone(), is(true));
+		}
 
 		NestedObject output = vpack.deserialize(slice, NestedObject.class);
 		assertThat(output.value, instanceOf(Map.class));
 
+		((Map<?, ?>) output.value).remove("_class");
 		assertThat(output.value, equalTo(input.value));
 	}
 
@@ -109,12 +174,13 @@ public class VPackWithoutTypeHintTest {
 		NestedGeneric<Collection<String>> input = new NestedGeneric<>();
 		input.value = Arrays.asList("a", "b", "c");
 
-		final VPack vpack = new VPack.Builder().useTypeHint(false).build();
+		final VPack vpack = new VPack.Builder().useTypeHint(useTypeHint).build();
 		final VPackSlice slice = vpack.serialize(input);
 
 		assertThat(slice.isObject(), is(true));
 		assertThat(slice.get("value").isArray(), is(true));
-		assertThat(slice.get("_class").isNone(), is(true));
+		if (!useTypeHint)
+			assertThat(slice.get("_class").isNone(), is(true));
 
 		NestedGeneric<?> output = vpack.deserialize(slice, NestedGeneric.class);
 		assertThat(output.value, instanceOf(List.class));
@@ -124,84 +190,51 @@ public class VPackWithoutTypeHintTest {
 
 	@Test
 	public void nestedGenericOfMapWithoutTypeInformation() {
-		NestedGeneric<Map<String, String>> input = new NestedGeneric<>();
-		input.value = Collections.singletonMap("a", "b");
+		Map<String, String> map = new HashMap<>();
+		map.put("a", "b");
 
-		final VPack vpack = new VPack.Builder().useTypeHint(false).build();
+		NestedGeneric<Map<String, String>> input = new NestedGeneric<>();
+		input.value = map;
+
+		final VPack vpack = new VPack.Builder().useTypeHint(useTypeHint).build();
 		final VPackSlice slice = vpack.serialize(input);
 
 		assertThat(slice.isObject(), is(true));
 		assertThat(slice.get("value").isObject(), is(true));
-		assertThat(slice.get("_class").isNone(), is(true));
-		assertThat(slice.get("value").get("_class").isNone(), is(true));
+		if (!useTypeHint) {
+			assertThat(slice.get("_class").isNone(), is(true));
+			assertThat(slice.get("value").get("_class").isNone(), is(true));
+		}
 
 		NestedGeneric<?> output = vpack.deserialize(slice, NestedGeneric.class);
 		assertThat(output.value, instanceOf(Map.class));
 
+		((Map<?, ?>) output.value).remove("_class");
 		assertThat(output.value, equalTo(input.value));
 	}
 
 	@Test
 	public void nestedGenericOfObjectWithoutTypeInformation() {
-		NestedGeneric<Object> input = new NestedGeneric<>();
-		input.value = Collections.singletonMap("a", "b");
+		Map<String, String> map = new HashMap<>();
+		map.put("a", "b");
 
-		final VPack vpack = new VPack.Builder().useTypeHint(false).build();
+		NestedGeneric<Object> input = new NestedGeneric<>();
+		input.value = map;
+
+		final VPack vpack = new VPack.Builder().useTypeHint(useTypeHint).build();
 		final VPackSlice slice = vpack.serialize(input);
 
 		assertThat(slice.isObject(), is(true));
 		assertThat(slice.get("value").isObject(), is(true));
-		assertThat(slice.get("_class").isNone(), is(true));
-		assertThat(slice.get("value").get("_class").isNone(), is(true));
+		if (!useTypeHint) {
+			assertThat(slice.get("_class").isNone(), is(true));
+			assertThat(slice.get("value").get("_class").isNone(), is(true));
+		}
 
 		NestedGeneric<?> output = vpack.deserialize(slice, NestedGeneric.class);
 		assertThat(output.value, instanceOf(Map.class));
 
-		assertThat(output.value, equalTo(input.value));
-	}
-
-	public static class NestedGenericBoundedCollection<T extends Collection<?>> {
-		public T value;
-	}
-
-	@Test
-	public void nestedGenericBoundedCollectionWithoutTypeInformation() {
-		NestedGenericBoundedCollection<Collection<String>> input = new NestedGenericBoundedCollection<>();
-		input.value = Arrays.asList("a", "b", "c");
-
-		final VPack vpack = new VPack.Builder().useTypeHint(false).build();
-		final VPackSlice slice = vpack.serialize(input);
-
-		assertThat(slice.isObject(), is(true));
-		assertThat(slice.get("value").isArray(), is(true));
-		assertThat(slice.get("_class").isNone(), is(true));
-
-		NestedGenericBoundedCollection<?> output = vpack.deserialize(slice, NestedGenericBoundedCollection.class);
-		assertThat(output.value, instanceOf(List.class));
-
-		assertThat(output.value, equalTo(input.value));
-	}
-
-	public static class NestedGenericBoundedMap<T extends Map<?, ?>> {
-		public T value;
-	}
-
-	@Test
-	public void nestedGenericBoundedMapWithoutTypeInformation() {
-		NestedGenericBoundedMap<Map<String, String>> input = new NestedGenericBoundedMap<>();
-		input.value = Collections.singletonMap("a", "b");
-
-		final VPack vpack = new VPack.Builder().useTypeHint(false).build();
-		final VPackSlice slice = vpack.serialize(input);
-
-		assertThat(slice.isObject(), is(true));
-		assertThat(slice.get("value").isObject(), is(true));
-		assertThat(slice.get("_class").isNone(), is(true));
-		assertThat(slice.get("value").get("_class").isNone(), is(true));
-
-		NestedGenericBoundedMap<?> output = vpack.deserialize(slice, NestedGenericBoundedMap.class);
-		assertThat(output.value, instanceOf(Map.class));
-
+		((Map<?, ?>) output.value).remove("_class");
 		assertThat(output.value, equalTo(input.value));
 	}
 
